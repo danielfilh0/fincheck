baebedd842b240cb2a081d57ba866fe0ee2fe3ac
Daniel Filho <danbsilva98@gmail.com>
2023-06-21 15:52:01 -0300

    feat: add user model and its migration

---
+++ prisma/migrations/20230621184757_create_users_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
