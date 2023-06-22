23f430564ec3f79defdcc7e7071fdc9310d75b22
Daniel Filho <danbsilva98@gmail.com>
2023-06-22 14:54:19 -0300

    feat: add category model and its migration

---
+++ prisma/migrations/20230622175355_create_categories_table/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "transaction_type" AS ENUM ('INCOME', 'EXPENSE');
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+    "type" "transaction_type" NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "categories" ADD CONSTRAINT "categories_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
