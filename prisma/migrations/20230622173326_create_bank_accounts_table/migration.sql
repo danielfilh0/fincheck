4169ced340ce6699350269cb2e11ed2e470fce49
Daniel Filho <danbsilva98@gmail.com>
2023-06-22 14:41:50 -0300

    feat: add bank account model and its migrations

---
+++ prisma/migrations/20230622173326_create_bank_accounts_table/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "BankAccountType" AS ENUM ('CHECKING', 'INVESTMENT', 'CASH');
+
+-- CreateTable
+CREATE TABLE "bank_accounts" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "initial_balance" DOUBLE PRECISION NOT NULL,
+    "type" "BankAccountType" NOT NULL,
+    "color" TEXT NOT NULL,
+
+    CONSTRAINT "bank_accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "bank_accounts" ADD CONSTRAINT "bank_accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
