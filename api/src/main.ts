bb83a29ee5279b158e9248dd2f4e6ee2b8ca6f49
Daniel Filho <danbsilva98@gmail.com>
2023-07-09 21:24:43 -0300

    feat: enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+  });
 
   await app.listen(3000);
 }
