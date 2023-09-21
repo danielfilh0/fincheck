09c7fa886f103812ff3dc046301e3698f04c3e12
Daniel Filho <danbsilva98@gmail.com>
2023-09-21 16:32:55 -0300

    feat(backend): add category data on findAllByUserId of transactions

--- api/src/modules/transactions/services/transactions.service.ts
+++ api/src/modules/transactions/services/transactions.service.ts
@@ -58,6 +58,15 @@ export class TransactionsService {
         bankAccountId: filters.bankAccountId,
         type: filters.type,
       },
+      include: {
+        category: {
+          select: {
+            id: true,
+            name: true,
+            icon: true,
+          },
+        },
+      },
     });
   }
 
