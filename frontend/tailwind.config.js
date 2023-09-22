5a3425680609d6720ab9e87e3b8b02fa3276fdca
Daniel Filho <danbsilva98@gmail.com>
2023-09-22 16:00:02 -0300

    feat(frontend): add DM Sans font-family

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -200,6 +200,9 @@ export default {
         'overlay-show': 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
         'content-how': 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
       },
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif']
+      }
     },
   },
   plugins: [],
