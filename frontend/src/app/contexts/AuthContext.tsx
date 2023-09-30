6254bf87d85044a1f1dea27dd427e333cf65170f
Daniel Filho <danbsilva98@gmail.com>
2023-09-30 15:44:59 -0300

    feat(frontend): add clear cache on logout

--- frontend/src/app/contexts/AuthContext.tsx
+++ frontend/src/app/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useCallback, useEffect, useState } from 'react'
 import { localStorageKeys } from '../config/localStorageKeys'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { usersService } from '../services/usersService'
 import { toast } from 'react-hot-toast'
 import { LaunchScreen } from '../../view/components/LaunchScreen'
@@ -29,6 +29,8 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     staleTime: Infinity
   })
 
+  const queryClient = useQueryClient()
+
   const signin = useCallback((acessToken: string) => {
     localStorage.setItem(localStorageKeys.ACCESS_TOKEN, acessToken)
 
@@ -38,6 +40,8 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   const signout = useCallback(() => {
     localStorage.removeItem(localStorageKeys.ACCESS_TOKEN)
     remove()
+
+    queryClient.clear()
 
     setSignedIn(false)
   }, [remove])
